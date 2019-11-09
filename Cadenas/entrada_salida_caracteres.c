b39912f5117aafee065b17a108393b1826626f4e
Francisco Bautista <jbautista@externos.santander.com.mx>
2019-11-09 16:47:39 -0600

    Ejercicio Cuenta Caracteres.

---
+++ Cadenas/entrada_salida_caracteres.c
@@ -0,0 +1,31 @@
+/**************************************************************
+ * @author Francisco Bautista (BAJJ)
+ * @email jbautista@externos.santander.com.mx
+ * @create date 2019-11-09 15:36:44
+ * @modify date 2019-11-09 15:36:44
+ * @desc Demostracion sobre funciones para manejo de caracteres
+ *       en los archivos de entrada y salida estandar.
+ **************************************************************/
+#include <stdio.h>
+
+/***************************************************************************
+ * @brief Funcion princial que demuestra el funcionamiento basico
+ *        de las funciones para manejo de caracteres de la 
+ *        biblioteca estandar.
+ *        getchar: lee un caracter del archivo de entrada estandar.
+ *        putchar: escribe un caracter dado, al archivo de salida estandar.
+ * @return int 
+ *****************************************************************************/
+int main(void) 
+{
+    char caracter;
+
+    // EOF: Constante que indica que se llego al final del archivo
+    //      Y por tanto ya no es posible seguir leyendo
+    printf("Ingrese lineas de texto a la entrada\n");
+    printf("Para observar demostracion de funciones\n");
+    printf("Para detener la ejecucion del programa presione Ctrl+c\n");
+    while((caracter = getchar()) != EOF)
+        putchar(caracter);
+    return 0;
+}
---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Ejercicios para aprendizaje de Lenguaje C
+
+## Tipos de variables
+| **Tipo de Variable** | **Longitud** |              **Valores** |
+| :------------------- | :----------: | -----------------------: |
+| *int*                |   4 bytes    |                  Enteros |
+| *float*              |   4 bytes    | Racionales, Irracionales |
+| *double*             |   8 bytes    |    Flotante de presicion |
+| *char*               |    1 byte    |         Caracteres ascii |
+
+> **Nota:** No se debe depender de las longitudes de los datos ya que estas pueden variar segun la implantacion.
+
+----
+
+## Estructuras de control y operadoeres
+
+----
+
+## Arreglos numericos
+
+-----
+
+## Arreglos de caracteres (Cadenas) y Constantes simbolicas
+
+Favor de compilar el archivo *./Cadenas/entrada_salida_caracteres.c* y comprobar la demostracion de funcionamiento de las funciones getchar y putchar.
+
+En caso de que existan dudas sobre el funcionamiento de lo demostrado, indicarlo en el Readme.md del Branch master.
+
+**TODO LIST**
+- [ ] Basado en la demostracion del archivo indicado, favor de crear el archivo *cuenta_caracteres.c* en el subdirectorio *Cadenas*
+
+> El programa debera imprimir los caracteres leidos y en otra linea el numero de caracteres leidos.
