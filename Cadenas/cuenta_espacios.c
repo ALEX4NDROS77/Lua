4d7121baaf377276b40d90ac60770780523cd076
does0jff <does0jff@gmail.com>
2019-12-05 20:38:59 -0600

    Observciones 20191205

--- Cadenas/cuenta_espacios.c
+++ Cadenas/cuenta_espacios.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+// BAJJ --> Las constantes simbolicas se declaran despues
+// de incluir las cabeceras
+
 int main(void)
 {
     char caracter;
     int cantidad = 0;
+    // BAJJ --> Los nombres de las constantes simbolicas se escrbien siempre en Mayuscula,
+    // las palabras se separan con '_'
     #define Espacio ' '
     #define NuevaLinea '\n'
 
@@ -20,6 +25,8 @@ int main(void)
             cantidad = 0;
         }
         //Trabajando en correcion de warning: "comparison between pointer and integer".
+        // BAJJ --> La variable caracter solo almacena un caracter a la vez, tu tratas de compararlo con multiples caracteres.
+        // La solucion seria comparar caracter por caracter la entrada hasta completar la palabra.
         if(caracter == "exit")
         {
             break;
