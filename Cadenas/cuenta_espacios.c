ddcc99518486a875ad0b2fe327d3c996f047caeb
ALEX4NDROS77 <alexymax34@hotmail.com>
2019-12-12 21:45:06 -0600

    Correciones121219

--- Cadenas/cuenta_espacios.c
+++ Cadenas/cuenta_espacios.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-// BAJJ --> Las constantes simbolicas se declaran despues
-// de incluir las cabeceras
-    #define ESPACIO ' '
-    #define NUEVA_LINEA '\n'
-
-    // Estas variables deberian estar declaradas dentro del bloque Main, no fuera de el.
-    // Esto que acabas de realizar se trata de variables globales al programa, trataremos el tema mas tarde
-    char caracter;
-    int cantidad = 0;
+#define ESPACIO ' '
+#define NUEVA_LINEA '\n'
+    
 int main(void)
 {
+    char caracter;
+    int cantidad = 0;
 
     while((caracter = getchar()) != EOF)
     {
@@ -29,13 +25,9 @@ int main(void)
         // por cada nuevo caracter que encuentra 
         if(caracter == 'e')
         {
-            // BAJJ --> aunque estas cerca tendras que pensar en una mejor manera de resolver el problema.
-            // esfuerzate ;)
             caracter = getchar();
             if(caracter =='x')
             {
-                // BAJJ --> Por ultimo, haz favor de probar tus modificaciones posterior a que las hagas, este programa no funciona
-                // en su estado actual
                 caracter = getchar();
                 if(caracter =='i')
                 {
--- Cadenas/duplica_o.c
+++ Cadenas/duplica_o.c
@@ -1,5 +1,8 @@
 // BAJJ --> Eliminar la marca implica tambien finalizar el ejercicio, pero yo lo veo igual que antes.
 #include <stdio.h>
+#define O 'o','O'
+#define NuevaLinea '\n'
+
 int main(void)
 {
     char caracter;
@@ -7,8 +10,6 @@ int main(void)
     // BAJJ --> Los comentarios sobre las constantes simbolicas son los mismo que en el ejercicio anterior
     // Se agrega adicionalmente que las constantes almacenan un texto de sustitucion.
     // El nombre de la constante deberia ser general, es decir que pueda se asignado cualquier valor no solo 'o'
-    #define O 'o','O'
-    #define NuevaLinea '\n'
 
     while((caracter = getchar()) != EOF)
     {
@@ -20,7 +21,6 @@ int main(void)
         // BAJJ --> Cual es el proposito de este bloque?
         if(caracter == NuevaLinea)
         {
-            //Trabajando para convertir el numero de 'o' contadas en 'o' replicadas.
             printf("%d",caracter);
         }
         
